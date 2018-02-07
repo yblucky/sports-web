559077247bb400bd63927785300b61901bb32dbb
yyr <1091945691@qq.com>
2018-02-07 23:28:42 +0800

    ok

--- src/pages/desktop/userOrder/userOrder.ts
+++ src/pages/desktop/userOrder/userOrder.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 declare var $: any;
 declare var layer: any;
-var stbExchangePage: any;
+var userOrderPage: any;
 
 @Component({
     selector   : 'page-userOrder',
@@ -14,22 +14,18 @@ var stbExchangePage: any;
 })
 export class UserOrderPage {
     find:any={
-      busnessType:"".trim(),
-      currencyType:"10".trim(),
-      uid:"".trim(),
-      mobile:"".trim(),
-      startTime:"".trim(),
-      endTime:"".trim(),
-
+      busnessType:[21,22]
     };
-    currencyTypes:any;
+
     orderData:any;
+    businessTypeDesc:string;
+    bettingContent:string = "";
     showTime:any = new Date();
     constructor(private router:Router,private httpService:HttpService,private aroute:ActivatedRoute,private utils:Utils) {
         this.aroute.params.subscribe( params  => {
             this.showTime = new Date();
         });
-        stbExchangePage=this;
+        userOrderPage=this;
         this.loadDataOne();
     }
 
@@ -46,7 +42,7 @@ export class UserOrderPage {
     */
     loadData(){
       this.httpService.pagination({
-          url:'/record/record',
+          url:'/record/list',
           data:this.find
       });
 
@@ -57,13 +53,32 @@ export class UserOrderPage {
       * 弹出等级面板
       */
       openDetail(item:any){
-          if(item.busnessType==31){
+          if(item.busnessType==31 || item.busnessType == 32){
             this.httpService.get({
                 url:'/record/racingBettingRecord',
                 data:{businessNumber:item.businessNumber}
             }).subscribe((data:any)=>{
                 if(data.code==='0000'){
                   this.orderData = data.data;
+                  this.businessTypeDesc = '北京赛车';
+                  this.bettingContent = "";
+                  for(var i = 0;i<data.data.length;i++){
+                    this.bettingContent = "";
+                    this.bettingContent += this.orderData[i].lotteryOne + ",";
+                    this.bettingContent += this.orderData[i].lotteryTwo + ",";
+                    this.bettingContent += this.orderData[i].lotteryThree + ",";
+                    this.bettingContent += this.orderData[i].lotteryFour + ",";
+                    this.bettingContent += this.orderData[i].lotteryFive + ",";
+                    this.bettingContent += this.orderData[i].lotterySix + ",";
+                    this.bettingContent += this.orderData[i].lotterySeven + ",";
+                    this.bettingContent += this.orderData[i].lotteryEight + ",";
+                    this.bettingContent += this.orderData[i].lotteryNine + ",";
+                    this.bettingContent += this.orderData[i].lotteryTen;
+                    this.bettingContent = this.bettingContent.replace(/-1/g,"-");
+                    this.orderData[i].bettingContent=this.bettingContent;
+                  }
+
+
                 }else if(data.code==='9999'){
                     Utils.show(data.message);
                 }else{
@@ -77,6 +92,18 @@ export class UserOrderPage {
             }).subscribe((data:any)=>{
                 if(data.code==='0000'){
                   this.orderData = data.data;
+                  this.businessTypeDesc = '时时彩';
+                  //this.bettingContent = "";
+                  for(var i = 0;i<data.data.length;i++){
+                      this.bettingContent = "";
+                      this.bettingContent += this.orderData[i].lotteryOne + ",";
+                      this.bettingContent += this.orderData[i].lotteryTwo + ",";
+                      this.bettingContent += this.orderData[i].lotteryThree + ",";
+                      this.bettingContent += this.orderData[i].lotteryFour + ",";
+                      this.bettingContent += this.orderData[i].lotteryFive;
+                      this.bettingContent = this.bettingContent.replace(/-1/g,"-");
+                      this.orderData[i].bettingContent=this.bettingContent;
+                  }
                 }else if(data.code==='9999'){
                     Utils.show(data.message);
                 }else{
@@ -84,6 +111,8 @@ export class UserOrderPage {
                 }
             });
           }
+
+
 
           layer.open({
               title: "投注明细",
@@ -94,7 +123,7 @@ export class UserOrderPage {
               fixed: true,
               shadeClose: false,
               resize: false,
-              area: ['550px','400px'],
+              area: ['880px','400px'],
               content: $("#editPanel"),
               yes: function(index:number){
                      layer.closeAll();
